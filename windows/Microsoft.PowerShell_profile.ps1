a03a99c8762a3c27374c89536e48b36cab2b7e69
Fabrizio Ervini <fabri.es018@gmail.com>
2024-01-04 11:12:26 +0100

    feat(windows): add PowerShell config

---
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,16 @@
+﻿oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\jandedobbeleer.omp.json" | Invoke-Expression
+
+# Alias
+New-Alias -Name clr -Value clear
+New-Alias -Name d -Value docker
+New-Alias -Name dc -Value docker-compose
+New-Alias -Name onc -Value GoToOnclusive
+New-Alias -Name vc -Value GoToVueComponents
+
+# Functions
+function GoToOnclusive { Set-Location ".\Repos\Onclusive" }
+function GoToVueComponents { Set-Location ".\Repos\vue-components" }
+
+# Modules
+Import-Module -Name Terminal-Icons
+
