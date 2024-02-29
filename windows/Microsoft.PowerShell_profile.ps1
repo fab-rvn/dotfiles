2c3cf325a9f55c5e6ee4ac361f98a4175b3ae8f8
Fabrizio Ervini <fabri.es018@gmail.com>
2024-02-29 10:32:05 +0100

    feat(windows): update PowerShell config

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -6,11 +6,20 @@ New-Alias -Name d -Value docker
 New-Alias -Name dc -Value docker-compose
 New-Alias -Name onc -Value GoToOnclusive
 New-Alias -Name vc -Value GoToVueComponents
+New-Alias -Name omp-update -Value UpgradeOhMyPosh
 
 # Functions
 function GoToOnclusive { Set-Location ".\Repos\Onclusive" }
 function GoToVueComponents { Set-Location ".\Repos\vue-components" }
+function UpgradeOhMyPosh {
+    try {
+        winget upgrade jandedobbeleer.OhMyPosh -s winget
+        Write-Host "Oh My Posh upgraded successfully!"
+    } catch {
+        Write-Error "Error upgrading Oh My Posh: $($_.Exception.Message)"
+        Exit-Function -Code 1
+    }
+}
 
 # Modules
 Import-Module -Name Terminal-Icons
-
